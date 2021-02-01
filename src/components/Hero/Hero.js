0f35ef78407a7b4e86df85ccbdc628626cdcb551
MGTriad <ProgrammerNammer@users.noreply.github.com>
2021-02-02 00:01:50 +0800

    Added proper attribution to the owner of the hero image

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -2,7 +2,22 @@ import React from "react";
 import "./Hero.css";
 
 const Hero = () => {
-  return <header>Hero</header>;
+  return (
+    <header>
+      <div>Main</div>
+      <div className="attributes">
+        Background image by Ramiltibayan.
+        <br />
+        <a
+          href="https://commons.wikimedia.org/wiki/File:St._La_Salle_Hall_Facade.JPG"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Ramiltibayan, CC BY-SA 4.0
+        </a>
+      </div>
+    </header>
+  );
 };
 
 export default Hero;
