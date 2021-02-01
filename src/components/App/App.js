69138735b17bc9856b97ddac78101818077777b6
MGTriad <ProgrammerNammer@users.noreply.github.com>
2021-02-01 23:26:19 +0800

    Added components and Hero background

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Hero from "../Hero/Hero";
+import "./App.css";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Hero />
+      </div>
+    );
+  }
+}
+
+export default App;
