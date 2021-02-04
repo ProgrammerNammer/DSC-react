0b823e6d26c6885787177912358c1f6934a81052
MGTriad <ProgrammerNammer@users.noreply.github.com>
2021-02-04 20:33:02 +0800

    Navigation bar slightly transparent

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Hero from "../Hero/Hero";
 import Navigation from "../Navigation/Navigation";
+import SectionAboutUs from "../SectionAboutUs/SectionAboutUs";
 import "./App.css";
 
 class App extends React.Component {
@@ -11,7 +12,7 @@ class App extends React.Component {
           <Navigation />
           <Hero />
         </header>
-        <section>Sample</section>
+        <SectionAboutUs />
       </div>
     );
   }
--- src/components/SectionAboutUs/SectionAboutUs.js
+++ src/components/SectionAboutUs/SectionAboutUs.js
@@ -3,9 +3,9 @@ import React from "react";
 class SectionAboutUs extends React.Component {
   render() {
     return (
-      <div>
+      <section>
         <h1> SECTION START </h1>
-      </div>
+      </section>
     );
   }
 }
