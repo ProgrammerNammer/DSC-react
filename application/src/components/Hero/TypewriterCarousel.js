1bbf29a7cce7235bf4578d67ce3936a5caaff67a
MGTriad <ProgrammerNammer@users.noreply.github.com>
2021-02-23 20:03:10 +0800

    Colored carousel

--- application/src/components/Hero/TypewriterCarousel.js
+++ application/src/components/Hero/TypewriterCarousel.js
@@ -1,22 +1,20 @@
-/*  TODO    Color rotation
- *
+/*
  *  Graham, G. (2017, December 14). Typewriter Effect. Retrieved
  *  from: css-tricks.com/snippets/css/typewriter-effect/#more
  */
 
 var colorRotation = ["#4285f4", "#ea4335", "#fbbc05", "#34a853"];
-
-var TxtType = function (el, toRotate, period, color) {
+var TxtType = function (el, toRotate, period) {
   this.toRotate = toRotate;
   this.el = el;
   this.loopNum = 0;
   this.period = parseInt(period, 10) || 2000;
   this.txt = "";
-  this.tick(color);
+  this.tick();
   this.isDeleting = false;
 };
 
-TxtType.prototype.tick = function (color) {
+TxtType.prototype.tick = function () {
   var i = this.loopNum % this.toRotate.length;
   var fullTxt = this.toRotate[i];
 
@@ -26,7 +24,13 @@ TxtType.prototype.tick = function (color) {
     this.txt = fullTxt.substring(0, this.txt.length + 1);
   }
 
-  this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
+  var j = this.loopNum % 4;
+  this.el.innerHTML =
+    '<span class="wrap" style="color: ' +
+    colorRotation[j] +
+    ';" >' +
+    this.txt +
+    "</span>";
 
   var that = this;
   var delta = 200 - Math.random() * 100;
@@ -54,16 +58,13 @@ window.onload = function () {
   for (var i = 0; i < elements.length; i++) {
     var toRotate = elements[i].getAttribute("data-type");
     var period = elements[i].getAttribute("data-period");
-    var color = colorRotation[i % 4];
-
     if (toRotate) {
-      new TxtType(elements[i], JSON.parse(toRotate), period, color);
+      new TxtType(elements[i], JSON.parse(toRotate), period);
     }
   }
-
   // INJECT CSS
   var css = document.createElement("style");
   css.type = "text/css";
-  css.innerHTML = `.typewrite > .wrap { border-right: 0.08em solid #fff }`;
+  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
   document.body.appendChild(css);
 };
