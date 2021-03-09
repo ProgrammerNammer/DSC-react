f01f6ab9ed3b835a9ba9fbc5842fefe13429d8ea
MGTriad <ProgrammerNammer@users.noreply.github.com>
2021-03-09 20:13:07 +0800

    Removed Redux-related files and folders

--- application/src/index.js
+++ application/src/index.js
@@ -1,17 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import thunk from "redux-thunk";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
 
 import App from "./components/App/App";
-import reducers from "./reducers";
 
-const store = createStore(reducers, applyMiddleware(thunk));
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.querySelector("#root")
-);
+ReactDOM.render(<App />, document.querySelector("#root"));
--- application/src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-
-const dummyReducer = (state = [], action) => {
-  return "DUMMY_REDUCER";
-};
-
-export default combineReducers({ dummyReducer });
